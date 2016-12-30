50a3a038a68bdff99fb6544196ffeb4edd44c54d
RolandLenovoDev <rolandbarro@gmail.com>
2016-12-30 20:55:31 +0800

    RepositoryCommit
    Partial

--- WaterBillingSystem/WaterBilling.Model/Member.cs
+++ WaterBillingSystem/WaterBilling.Model/Member.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace WaterBilling.Model
 {
-    public class Member
+    
+    public partial class Member : IEntity
     {
-        public int Id { get; set; }
+        public new int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
